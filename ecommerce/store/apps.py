143fb04d66fe38719fbf870d40c88d25359a2027
kiss Tibor <kisss.tibi@gmail.com>
2024-10-12 21:07:38 +0200

    rendelés megjelenítése admin oldalon

--- ecommerce/store/apps.py
+++ ecommerce/store/apps.py
@@ -5,5 +5,7 @@ class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
 
+    
+
     def ready(self):
         import store.signals  # Signal regisztrálása
