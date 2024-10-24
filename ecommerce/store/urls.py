cfc74600e77f37c90e478614cc0033703d0b679b
Kiss Tibor <kisss.tibi@gmail.com>
2024-10-24 23:16:42 +0200

    rendezés név és ár szerint

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,37 +1,3 @@
-'''
-from django.urls import path, include
-from . import views
-from .views import register
-from .views import product_detail
-
-# saját
-from .views import CustomLoginView  # Importáljuk a nézetet
-from .views import register
-from django.contrib.auth import views as auth_views # kijelentkezés
-from .views import CustomLoginView, register
-from .views import order_list
-from .views import ProductList
-
-urlpatterns = [
-    path('', views.store, name="store"),
-    path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout"),
-    path('login/', CustomLoginView.as_view(), name='login'),  # Bejelentkezési URL
-    path('register/', views.register, name='register') , #regisztrációs url
-    path('update_item/', views.updateItem, name="update_item"),
-    path('process_order/', views.processOrder, name="process_order"),
-    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout', kwargs={'next_page': 'store'}, ),  # home az átirányítás célja
-    path('orders/', views.order_list, name='order_list'),  # Rendelések listája
-    path('api/products/', ProductList.as_view(), name='product-list'),
- 
-    
-   
-
-]
-'''
-
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
@@ -46,7 +12,9 @@ from .views import (
     order_list, 
     ProductList,
     filtered_products,
-    index
+    index,
+    reset_filters
+
 )
 
 urlpatterns = [
@@ -64,6 +32,7 @@ urlpatterns = [
     path('api/products/', ProductList.as_view(), name='product-list'),  # API a termékekhez
     path('search/', views.search, name='search'),
     path('filtered-products/', filtered_products, name='filtered_products'),
+    path('reset-filters/', reset_filters, name='reset_filters'),
 ]
 
 if settings.DEBUG:
